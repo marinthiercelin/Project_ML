6a8c0fef56ed7b73ca6d5bf3eb59f8ab1f0f2bf2
Romain Artru <romain.artru@epfl.ch>
2017-10-23 18:24:15 +0200

    added comments for all functions. If a comment was already there I didn't modify it.

--- firstProject/code/draw2DMedian.py
+++ firstProject/code/draw2DMedian.py
@@ -1,3 +1,11 @@
+"""Draw a plot for all the data cleaned.
+
+Load a subset of the data from the train.csv file.
+Clean the data with the function fillMissingValuesMedianWithY.
+Draw all plots of the clean data (1 in function of all other).
+Save those drawing in ../data/Figures/2DMedian.
+"""
+
 import Helpers.helpers as helper
 import Visualization.visu as visu
 import cleaner
