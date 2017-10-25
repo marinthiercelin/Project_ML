09d6bd2f6c3d5928dcd1248ccb322646cc93045a
Leonardo Aoun <aounleo@tsf-428-wpa-1-137.epfl.ch>
2017-10-25 15:01:42 +0200

    bou

---
+++ firstProject/code/clean.py
@@ -0,0 +1,16 @@
+import numpy as np
+import Helpers.helpers as helper
+from Helpers.cleaner import *
+
+def main():
+    y,x, ids = helper.load_csv_data('../data/train.csv', False)
+
+    x = filter_bad_features(x)
+    x, y = bad_samples = filter_bad_samples(x, y)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
