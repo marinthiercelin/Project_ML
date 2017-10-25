48d9d580704dfa0cbad2086e5a66dd81c429592b
Marin THIERCELIN <marin.thiercelin@epfl.ch>
2017-10-25 15:03:52 +0200

    added function to remove features

--- firstProject/code/cleaner.py
+++ firstProject/code/cleaner.py
@@ -148,25 +148,10 @@ def outliersToMedian(x):
         x_col[np.where(x_col > upBound)] = median
     return x
 
-#useless, they are all outliers for something
-def deleteOutliers(x):
-    toDel = np.array([])
-    for col in range(x.shape[1]):
-        x_col = x[:,col]
-        column = x_col[np.where(x_col != -999)]
-        median = np.median(column)
-        quInf = np.percentile(column,25)
-        quSup = np.percentile(column,75)
-        woUnknown = np.copy(x_col)
-        woUnknown[np.where(woUnknown == -999)] = median
-        iqr = quSup - quInf
-        loBound = quInf - 1.5*iqr
-        upBound = quSup + 1.5*iqr
-        toDel = np.union1d(toDel, np.where(woUnknown < loBound)[0])
-        toDel = np.union1d(toDel, np.where(woUnknown > upBound)[0])
-    print(toDel.shape[0],x.shape[0])
-    x = np.delete(x,toDel,axis=0)
-    return x
+def remove_unused_features(x,toRemove):
+    """filter out the features"""
+    xBis = np.delete(x,toRemove,axis=1)
+    return xBis
 
 #compute and print correlation
 def computeCorrelation(x):
